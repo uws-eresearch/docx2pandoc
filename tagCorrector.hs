dbba2c60c071100d9e9c61939c554c33c3aa77e6
Jesse Rosenthal <jrosenthal@jhu.edu>
2014-05-14 11:26:33 -0400

    Added a tag correcting script.

---
+++ tagCorrector.hs
@@ -0,0 +1,20 @@
+import Text.Pandoc.JSON
+import Data.List
+
+correctSpan :: Inline -> [Inline]
+correctSpan (Span ("", [], []) ils) = ils
+correctSpan (Span (ident, classes, kvs) ils)
+  | "emph" `elem` classes =
+    [Emph $ correctSpan $ Span (ident, (delete "emph" classes), kvs) ils]
+  | "strong" `elem` classes =
+    [Strong $ correctSpan $ Span (ident, (delete "strong" classes), kvs) ils]
+  | "smallcaps" `elem` classes =
+    [SmallCaps $ correctSpan $ Span (ident, (delete "smallcaps" classes), kvs) ils]
+  | "strikeout" `elem` classes =
+    [Strikeout $ correctSpan $ Span (ident, (delete "strikeout" classes), kvs) ils]
+  | otherwise =
+      [Span (ident, classes, kvs) ils]
+correctSpan il = [il]
+
+main = toJSONFilter correctSpan
+
