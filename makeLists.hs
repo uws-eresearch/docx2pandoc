ff65eecfe392201b240674a83c36d8058f3580af
Jesse Rosenthal <jrosenthal@jhu.edu>
2014-05-29 07:33:40 -0400

    Fixed list maker.

--- makeLists.hs
+++ makeLists.hs
@@ -8,47 +8,40 @@ isListItem :: Block -> Bool
 isListItem (Div (_, classes, _) _) | "list-item" `elem` classes = True
 isListItem blk = False
 
-getLevel :: Block -> Maybe Int
+getLevel :: Block -> Maybe Integer
 getLevel b@(Div (_, _, kvs) _) =  liftM read $ lookup "level" kvs
 getLevel b = Just (-1)
 
-getLevelN :: Block -> Int
+getLevelN :: Block -> Integer
 getLevelN b = case getLevel b of
   Just n -> n
   Nothing -> -1
 
-getNumId :: Block -> Maybe Int
+getNumId :: Block -> Maybe Integer
 getNumId b@(Div (_, _, kvs) _) =  liftM read $ lookup "num-id" kvs
 
-getNumIdN :: Block -> Int
+getNumIdN :: Block -> Integer
 getNumIdN b = case getNumId b of
   Just n -> n
   Nothing -> -1
 
-getBlocks :: Block -> [Block]
-getBlocks (Div (_, _, _) blks) = blks
-getBlocks blk = [blk]
+flatToBullets' :: Integer -> [(Integer, Block)] -> [Block]
+flatToBullets' _ [] = []
+flatToBullets' num xs@((n, b) : elems)
+  | n == num = b : (flatToBullets' num elems)
+  | otherwise = 
+    let (children, remaining) = span (\(m, _) -> m > num) xs
+    in
+     (BulletList [flatToBullets' n children]) : (flatToBullets' num remaining)
 
-data ItemList a = Blah a | Item a [ItemList a]
+flatToBullets :: [(Integer, Block)] -> [Block]
+flatToBullets elems = flatToBullets' (-1) elems
 
-
-listToTree :: [Block] -> [ItemList Block]
-listToTree (blk:ls) | (not . isListItem) blk = (Blah blk) : (listToTree ls)
-listToTree (blk:ls) = let
-      (children, rest) = span (\b -> (getLevelN b) > (getLevelN blk)) ls
-      subtrees = listToTree children
-   in Item blk subtrees : listToTree rest
-listToTree [] = []
-
-itemToBullet :: ItemList Block -> Block
-itemToBullet (Blah blk) = blk
-itemToBullet (Item blk subLsts) = BulletList [[blk] ++ (map itemToBullet subLsts)]
-
-foo :: [Block] -> [Block]
-foo blks = map itemToBullet $ listToTree blks
-
-bar :: Pandoc -> Pandoc
-bar p = bottomUp foo p
+blocksToBullets :: [Block] -> [Block]
+blocksToBullets blks = flatToBullets $ map (\b -> (getLevelN b, b)) blks
+  
+pandocToBullets :: Pandoc -> Pandoc
+pandocToBullets (Pandoc m blks) = Pandoc m (blocksToBullets blks)
 
 
-main = toJSONFilter bar
+main = toJSONFilter pandocToBullets
