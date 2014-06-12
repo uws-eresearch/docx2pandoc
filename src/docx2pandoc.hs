1614ada707d6fbb8025b81693c458ea74b6586b7
Jesse Rosenthal <jrosenthal@jhu.edu>
2014-06-12 13:13:35 -0400

    Have toplevel wrapper use `readDocX`

--- src/docx2pandoc.hs
+++ src/docx2pandoc.hs
@@ -1,15 +1,14 @@
 import Text.Pandoc.Readers.DocX
-import Codec.Archive.Zip
 import qualified Data.ByteString.Lazy as B
 import Text.Pandoc.JSON
 import Text.Pandoc
 import qualified Text.Pandoc.UTF8 as UTF8
 import System.Environment
 
-mkPandoc :: FilePath -> IO (Maybe [Block])
+mkPandoc :: FilePath -> IO Pandoc
 mkPandoc fp = do
   f <- B.readFile fp
-  return $ archiveToBlocks (toArchive f) 
+  return $ readDocX def f
 
 main :: IO ()
 main = do
@@ -17,10 +16,8 @@ main = do
   let fp = case args of
         [] -> error "A docx file is required"
         _  -> head args
-  output <- mkPandoc fp
-  case output of
-    Just blks -> UTF8.putStr $ writeJSON def (Pandoc nullMeta blks)
-    Nothing   -> error ("Couldn't parse docx file " ++ fp)
+  pandoc <- mkPandoc fp
+  UTF8.putStr $ writeJSON def pandoc
 
     
 
